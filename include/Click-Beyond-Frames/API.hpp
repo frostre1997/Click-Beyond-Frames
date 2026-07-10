c8ce640b8db780909e7a4b369161cb31138fe2e8
Frostre_ <n9043395@gmail.com>
2026-07-10 17:12:04 +0200

    Add API header for Click Beyond Frames

--- include/Click-Beyond-Frames/API.hpp
+++ include/Click-Beyond-Frames/API.hpp
@@ -1,1 +1,15 @@
+#pragma once
+#include <Geode/Result.hpp>
+#include <string>
 
+namespace clickbeyondframes::dev {
+
+    // Returns true if Click Beyond Frames is installed (this header existing implies it)
+    inline bool isInstalled() {
+        return true;
+    }
+
+    // Returns the current mod version
+    std::string getVersion();
+
+}
